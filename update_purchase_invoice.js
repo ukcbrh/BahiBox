83a9cc66a0b439dbcbf44e226e34c693dadedccb
BahiBox Dev <you@example.com>
2026-09-13 12:57:14 +0000

    Remove temp script

--- update_purchase_invoice.js
+++
@@ -1,9 +0,0 @@
-const fs = require('fs');
-const file = './src/components/retail/CreatePurchaseInvoice.tsx';
-let content = fs.readFileSync(file, 'utf8');
-content = content.replace(
-  /p_supplier_id: supplierId,\s+p_po_number: invoiceNo,/,
-  "p_supplier_id: supplierId,\n        p_invoice_number: invoiceNo,\n        p_invoice_date: invoiceDate,\n        p_po_number: invoiceNo,"
-);
-fs.writeFileSync(file, content);
-console.log('done');
